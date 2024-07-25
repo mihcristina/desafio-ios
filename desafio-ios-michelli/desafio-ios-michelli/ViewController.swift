914dc05206d38562a22a4ac66480c6209f89f2a6
Michelli Cristina de Paulo Lima <michellicristinadepaulolima@MacBook-Air-de-Michelli.local>
2024-07-25 11:14:33 -0300

    retira storyboard e configura viewcode

---
+++ desafio-ios-michelli/desafio-ios-michelli/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  desafio-ios-michelli
+//
+//  Created by Michelli Cristina de Paulo Lima on 25/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+}
